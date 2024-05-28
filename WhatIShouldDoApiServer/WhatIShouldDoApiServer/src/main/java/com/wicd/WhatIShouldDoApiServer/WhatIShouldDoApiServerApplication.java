8a40b6da7e990c9650eb7d818d84e31fced92b84
HyukYoon <56344824+hyukyyy@users.noreply.github.com>
2024-05-28 17:30:53 +0900

    api server initial commit

---
+++ WhatIShouldDoApiServer/WhatIShouldDoApiServer/src/main/java/com/wicd/WhatIShouldDoApiServer/WhatIShouldDoApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.wicd.WhatIShouldDoApiServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIShouldDoApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatIShouldDoApiServerApplication.class, args);
+	}
+
+}
---
+++ WhatIShouldDoApiServer/WhatIShouldDoApiServer/src/test/java/com/wicd/WhatIShouldDoApiServer/WhatIShouldDoApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wicd.WhatIShouldDoApiServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIShouldDoApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
